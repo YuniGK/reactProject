0763969c526fd6615b1de3a0424b9659482cddbe
kim yunjung <1106qwe@hanmail.net>
2024-11-03 17:19:44 +0900

    컴포넌트 생성

--- react-game/game/src/App.js
+++ react-game/game/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+//생성한 컴포넌트 가져오기
+import Box from './component/Box';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Box />
+      <Box />
+    </div>
   );
 }
 
---
+++ react-game/game/src/component/Box.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//rafce - 기본틀 자동완성 키워드
+const Box = () => {
+  return (
+    <div>
+        <h1>대상</h1>
+
+        <img src="" />
+        <h2>승패</h2>
+    </div>
+  )
+}
+
+//생성한 컴포넌트 내보내기
+export default Box
