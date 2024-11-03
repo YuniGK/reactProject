e5fe03b5026e78920312260e94c32eeffd9dd676
kim yunjung <1106qwe@hanmail.net>
2024-11-03 19:31:38 +0900

    값이 없을 경우를 대비

--- react-game/game/src/component/Box.js
+++ react-game/game/src/component/Box.js
@@ -1,14 +1,20 @@
 import React from 'react'
-//rafce - 기본틀 자동완성 키워드
-const Box = () => {
-  return (
-    <div>
-        <h1>대상</h1>
 
-        <img src="" />
-        <h2>승패</h2>
-    </div>
-  )
+//rafce - 기본틀 자동완성 키워드
+const Box = (props) => {
+    console.log(props)
+
+    return (
+        <div className='box'>
+            <h1>{props.title}</h1>
+
+            <div className='img-box'>
+                {/* item 없을 경우 이미지를 불러오지 않게 하기 위해서 아래와 같이 설정 */}
+                <img src={props.item && props.item.img} />
+            </div>
+            <h2>승패</h2>
+        </div>
+    )
 }
 
 //생성한 컴포넌트 내보내기
