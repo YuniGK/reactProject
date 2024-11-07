1d6d4dda1f10ceac835923d92b01f3f64c22b30b
kim yunjung <1106qwe@hanmail.net>
2024-11-07 22:09:53 +0900

    파라미터 useParams

--- router/router2/src/page/ProdcutDetailPage.js
+++ router/router2/src/page/ProdcutDetailPage.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 const ProdcutDetailPage = () => {
+    //파라미터 보내기
+    const params = useParams();
+    console.log('params ',params);
+    //params {id : 1}
   return (
     <div>
         <h2>ProdcutDetailPage</h2>
