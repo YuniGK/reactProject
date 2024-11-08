6495bd260a2ebff115b91aeaf2f16c23567142b4
kim yunjung <1106qwe@hanmail.net>
2024-11-08 22:23:20 +0900

    json-server / db.json설정

--- first-shop/shop/src/page/ProductAll.js
+++ first-shop/shop/src/page/ProductAll.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
 const ProductAll = () => {
   return (
     <div>
-        ProductAll
-        <FontAwesomeIcon icon={faCoffee} />
+
     </div>
   )
 }
