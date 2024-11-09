a633ecd60ccc76f333a3e96261425025b4b71ba6
kim yunjung <1106qwe@hanmail.net>
2024-11-09 19:02:51 +0900

    예전버전 redux 설정 및 적용

--- redux/redux-game/src/App.js
+++ redux/redux-game/src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+
+  //필요한 값을 불러온다.
+                            //스토어의 값들을 가져온다. 그중 count를 가져온다.
+  const count = useSelector(state => state.count);
 
   const increase = () => {
-    setCount(count+1);
+    //액션을 넣는다. 액션은 type(액션의 이름), payload(선택사항)로 구성된다.
+    //dispatch({type : , payload : })
+    dispatch({type : "INCREMENT"})
   }
 
   return (
