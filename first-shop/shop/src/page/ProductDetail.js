509a8284dce8aed04c2c66dd20710b3c87d2adc1
kim yunjung <1106qwe@hanmail.net>
2024-11-09 15:37:18 +0900

    useParams / json-server 를 이용해서 데이터 가져오기

--- first-shop/shop/src/page/ProductDetail.js
+++ first-shop/shop/src/page/ProductDetail.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
 
 const ProductDetail = () => {
+  const [producItem, setProducItem] = useState([]);
+
+  const {id} = useParams();
+
+  const getProduct = async () => {
+    let url = `http://localhost:4000/products/${id}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    
+    setProducItem(data);
+  };
+  
+  useEffect(()=>{
+    getProduct();
+  },[]);
+
   return (
-    <div>ProductDetail</div>
+    <div>
+
+    </div>
   )
 }
 
