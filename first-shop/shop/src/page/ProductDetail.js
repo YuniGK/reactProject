2a96019d5546c5bcb01455814d50e13399bd2570
kim yunjung <1106qwe@hanmail.net>
2024-11-09 15:53:01 +0900

    상품 상세

--- first-shop/shop/src/page/ProductDetail.js
+++ first-shop/shop/src/page/ProductDetail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { useParams } from 'react-router';
 
 const ProductDetail = () => {
@@ -11,7 +12,7 @@ const ProductDetail = () => {
 
     let response = await fetch(url);
     let data = await response.json();
-    
+
     setProducItem(data);
   };
   
@@ -20,10 +21,32 @@ const ProductDetail = () => {
   },[]);
 
   return (
-    <div>
+      <Container>
+        <Row>
+            <Col>
+              <img src={producItem?.img} alt={producItem?.title} />
+            </Col>
+            <Col>
+              <div>
+                <h3>{producItem?.title}</h3>
+                <p>{producItem?.price}</p>
+                <p className='sticker'>{producItem?.choice} {producItem?.new}</p>
 
-    </div>
+                <Form.Select aria-label="Default select example">
+                  <option>size select</option>
+                  {
+                    producItem.size?.map((size, idx)=>(
+                      <option value={size} key={idx} >{size}</option>
+                    ))
+                  }
+                </Form.Select>
+
+                <Button variant="dark">Cart</Button>
+              </div>
+            </Col>
+        </Row>
+      </Container>
   )
-}
+};
 
 export default ProductDetail
