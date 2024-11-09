ef8eb2ea1282d4bedf2ad0bdfe2df9c123b7ed64
kim yunjung <1106qwe@hanmail.net>
2024-11-09 18:40:55 +0900

    -

--- redux/redux-game/src/App.js
+++ redux/redux-game/src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count+1);
+  }
+
   return (
     <div>
+      <h3>{count}</h3>
+      <button onClick={increase}>증가</button>
     </div>
   );
 }
