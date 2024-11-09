ef44f82ef8584c954247d12a27676c5c0b60c311
kim yunjung <1106qwe@hanmail.net>
2024-11-09 16:36:37 +0900

    json server 버전 차이에 의한 검색 되지 않음
    npm uninstall -g json-server 서버 삭제 후
    npm install -g json-server@0.17.0 <- 원하는 버전으로 변경

--- first-shop/shop/src/page/ProductAll.js
+++ first-shop/shop/src/page/ProductAll.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import ProductCard from '../component/ProductCard';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
 
   const getProducts = async () => {
-    let url = 'http://localhost:4000/products';
+    let searchQuery = query.get('q') || "";
+    let url = `http://localhost:4000/products?q=${searchQuery}`;
 
     let response = await fetch(url);
     let data = await response.json();
-
+    console.log(url)
+    console.log(data)
     setProductList(data);
   };
   
-  useEffect(()=>{
+  useEffect(() => {
     getProducts();
-  },[]);
+  }, [query]);
 
   return (
     <div className='product-all'>
