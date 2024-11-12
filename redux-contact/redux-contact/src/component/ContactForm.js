4c40061786e436c7f365214d229063ae0e266078
kim yunjung <1106qwe@hanmail.net>
2024-11-12 20:12:46 +0900

    리팩토링 후 / 함수를 여러개 만든 과정 줄임

--- redux-contact/redux-contact/src/component/ContactForm.js
+++ redux-contact/redux-contact/src/component/ContactForm.js
@@ -5,29 +5,17 @@ const ContactForm = () => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState(0);
 
-    const getName = (event) => {
-        let value = event.target.value;
-
-        setName(value);
-    }
-
-    const getPhoneNumber = (event) => {
-        let value = event.target.value;
-
-        setPhoneNumber(value);
-    }
-
   return (
     <div>
         <Form>
             <Form.Group className="mb-3" controlId="formName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Name" onChange={getName} />
+                <Form.Control type="text" placeholder="Enter Name" onChange={(event)=>setName(event.target.value)} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formPhoneNumber">
                 <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="number" placeholder="Enter Phone Number" onChange={getPhoneNumber} />
+                <Form.Control type="number" placeholder="Enter Phone Number" onChange={(event)=>setName(event.target.value)} />
             </Form.Group>
        
             <Button variant="primary" type="submit">
