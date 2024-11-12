421e228dd53f0111766509a4edf78b832ce3bdbe
kim yunjung <1106qwe@hanmail.net>
2024-11-12 22:28:48 +0900

    검색 기능

--- redux-contact/redux-contact/src/component/SearchBox.js
+++ redux-contact/redux-contact/src/component/SearchBox.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Col, Form, Row } from 'react-bootstrap'
+import { useDispatch } from 'react-redux';
 
 const SearchBox = () => {
+  const [name, setName] = useState('');
+  const dispatch = useDispatch();
+  
+  const searchByName = (e) => {
+    e.preventDefault();
+    dispatch({type: "SEARCH_CONTACT", payload : {name}});
+}
   return (
     <div className='search-box'>
-        <Row>
-            <Col lg={10}><Form.Control type="text" placeholder="Enter Name" /></Col>
-            <Col lg={2}><Button variant="primary">search</Button></Col>
-        </Row>    
+        <Form onSubmit={searchByName}>
+          <Row>
+              <Col lg={10}><Form.Control type="text" placeholder="Enter Name" onChange={(event) => setName(event.target.value)}/></Col>
+              <Col lg={2}><Button variant="primary" type='submit'>search</Button></Col>
+          </Row>    
+        </Form>
     </div>
   )
 }
--- redux-contact/redux-contact/src/redux/reducer/reducer.js
+++ redux-contact/redux-contact/src/redux/reducer/reducer.js
@@ -2,10 +2,12 @@ import ContactList from "../../component/ContactList";
 
 let initialState = {
     contactList : []
+    , name : ''
 };
 
 function reducer(state=initialState, action){
     const {type, payload} = action;//호출을 통해 action.type
+
     switch(type){//action.type -> type로 줄인다.
         case "ADD_CONTACT" :
             //state값에 추가로 내용을 붙여준다.
@@ -18,6 +20,8 @@ function reducer(state=initialState, action){
                     }
                 ]
             };
+        case "SEARCH_CONTACT" :
+            return{ ...state, name: payload.name };
         default : 
             return{...state}
     }
