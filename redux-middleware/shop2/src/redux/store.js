b627664127960b87317ea95e2aa4916df2e66222
kim yunjung <1106qwe@hanmail.net>
2024-11-13 22:33:04 +0900

    최신 리덕스로 변경 1

---
+++ redux-middleware/shop2/src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import authenticateReducer from "./reducers/authenticateReducer"
+import productReducer from "./reducers/productReducer"
+
+const store = configureStore({
+    reducer : {
+        auth : authenticateReducer
+        , product : productReducer
+    }
+});
+
+export default store;
