f55277d7e0b4c15d9b3546400f836b8f9a43a3c5
kim yunjung <1106qwe@hanmail.net>
2024-11-18 21:53:43 +0900

    ReactQuery 라이프 사이클

--- react-query/react-query/src/page/ReactQueryPage.js
+++ react-query/react-query/src/page/ReactQueryPage.js
@@ -22,10 +22,19 @@ const ReactQueryPage = () => {
         , select: data => {
             return data.data//data값을 가져올 때, data값만 가져온다.
         }
-        , gcTime: 5000//캐시 관리하는 시간 5초가 지나면 캐시를 삭제한다.
+        , gcTime: 5000//캐시 관리하는 시간 5초가 지나면 캐시를 삭제한다. -> 기본값은 5분
         /* state 상태에서는 캐시가 삭제되지 않는다. 다른페이지로 이동 시, 상태가
         inactive로 변경되면서 5초가 지나면 캐시가 삭제된다. 
-        하위버전에서는 cacheTime라고 사용됨 */
+        하위버전에서는 cacheTime라고 사용됨 
+        
+        ReactQuery 라이프 사이클
+        fetching - api 호출 시
+        fresh - 데이터 도착 시, 해당 상태에서는 api 호출을 다시 하지 않는다.
+            해당 상태를 길게 두면 api 호출이 아닌 캐시의 데이터를 가져온다.
+        state - 데이터 도찰 후 시간이 지난 상태, 해당 상태에서 다시 api호출이 가능하다.
+        inactive - 데이터를 사용하지 않는 상태
+        deleted 
+        */
     });
 
     console.log('isLoading ', isLoading, ' --- ', data)
