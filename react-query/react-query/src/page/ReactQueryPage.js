d6d47c18225020630b14f37afa84d0dd920f980c
kim yunjung <1106qwe@hanmail.net>
2024-11-18 21:48:28 +0900

    gcTime 가비지 타임

--- react-query/react-query/src/page/ReactQueryPage.js
+++ react-query/react-query/src/page/ReactQueryPage.js
@@ -22,6 +22,10 @@ const ReactQueryPage = () => {
         , select: data => {
             return data.data//data값을 가져올 때, data값만 가져온다.
         }
+        , gcTime: 5000//캐시 관리하는 시간 5초가 지나면 캐시를 삭제한다.
+        /* state 상태에서는 캐시가 삭제되지 않는다. 다른페이지로 이동 시, 상태가
+        inactive로 변경되면서 5초가 지나면 캐시가 삭제된다. 
+        하위버전에서는 cacheTime라고 사용됨 */
     });
 
     console.log('isLoading ', isLoading, ' --- ', data)
