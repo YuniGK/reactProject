986254a8eb4b8bed4b68e8230e81343e6a7d0697
kim yunjung <1106qwe@hanmail.net>
2024-11-25 20:43:36 +0900

    장르 id 찾기

--- react-netflx/netflx/src/common/MovieCard/MovieCard.jsx
+++ react-netflx/netflx/src/common/MovieCard/MovieCard.jsx
@@ -3,8 +3,21 @@ import './MovieCard.style.css'
 import { Badge } from 'react-bootstrap'
 import { faPeopleGroup, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre'
 
 const MovieCard = ({movie}) => {
+  //data의 이름을 genreDat로 재정의한다.
+  const {data:genreData} = useMovieGenreQuery();
+  const showGenre = (genreIdList) => {
+    if(!genreData) return [];
+
+    const genreNameList = genreIdList.map((id)=>{
+      const genreObj = genreData.find((genre) => (genre.id === id))
+      return genreObj.name;
+    });
+    return genreNameList;
+  }
+
   return (
     <div className='movie-card'>
       <div className='movie-img' style={{backgroundImage : "url("+`https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`+")"}}></div>
@@ -12,7 +25,7 @@ const MovieCard = ({movie}) => {
       <div className='movie-slider-info'>
         <h2>{movie.title}</h2>
         <div className='movie-id'>
-            {movie.genre_ids.map((id, idx) => (
+            {showGenre(movie.genre_ids).map((id, idx) => (
               <Badge bg="primary" key={idx}>{id}</Badge>
             ))}
         </div>
