b1fe3d992b5d7113736685fa3f68b97eff045e22
kim yunjung <1106qwe@hanmail.net>
2024-11-26 20:44:22 +0900

    페이지네이션

--- react-netflx/netflx/src/pages/Movies/MoviePage.jsx
+++ react-netflx/netflx/src/pages/Movies/MoviePage.jsx
@@ -5,14 +5,17 @@ import { useSearchMovieQuery } from '../../hooks/useSearchMovie';
 import { useSearchParams } from 'react-router-dom';
 import MovieCard from '../../common/MovieCard/MovieCard';
 
+import ReactPaginate from 'react-paginate';
+
 const MoviePage = () => {
   const [query, setQuery] = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [page, setPage] = useState(1);
+
   const keyword = query.get('q');
 
-  const [loading, setLoading] = useState(true);
-  const {data, isLoading, isError, error} = useSearchMovieQuery({keyword});
-
-  console.log(data)
+  const {data, isLoading, isError, error} = useSearchMovieQuery({keyword, page});
 
     if(isLoading){
       <ClipLoader
@@ -28,6 +31,10 @@ const MoviePage = () => {
       <Alert key="danger" variant="danger">{error.message}</Alert>
     }
 
+    const handlePageClick = ({selected}) => {
+      setPage(selected+1);
+    };
+
   return (
     <Container>
       <Row>
@@ -38,6 +45,28 @@ const MoviePage = () => {
               <Col key={index} lg={4} xs={12}><MovieCard movie={movie} /></Col>
             ))}
           </Row>
+          {/* https://codepen.io/monsieurv/pen/abyJQWQ */}
+          <ReactPaginate
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={2}
+            pageCount={data?.total_pages}
+            previousLabel="< previous"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+            forcePage={page-1}
+          />
         </Col>
       </Row>
     </Container>
