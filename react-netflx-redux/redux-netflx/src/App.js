bedac3a3cffa20eb62793260efad6b4c74d91c46
kim yunjung <1106qwe@hanmail.net>
2024-11-27 20:23:04 +0900

    라우터 설정

--- react-netflx-redux/redux-netflx/src/App.js
+++ react-netflx-redux/redux-netflx/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/movies' element={<MoviePage />} />
+          <Route path='/movies/:id' element={<MovieDetailPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ react-netflx-redux/redux-netflx/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HomePage.style.css'
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
